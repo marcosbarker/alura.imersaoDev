59ec9362a7b4df0594336ee24663b3d650e81e45
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-03-26 09:55:18 -0300

    first commit

---
+++ 4_aluraflix(arrays)/setup.js
@@ -0,0 +1,10 @@
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BN2VlN2VhZWUtNjJlYi00NjI1LTk5NGQtZTE0YzA2YjZiN2NlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_UX182_CR0,0,182,268_AL_.jpg",
+                   "https://m.media-amazon.com/images/M/MV5BMWYwMWQxMmUtNjU3MS00NjIwLTllYWMtNzcxYzRkMWYyYTkwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                   "https://m.media-amazon.com/images/M/MV5BODEyOWY2ZDctOGI3Zi00YjYwLWIwZDEtMzJlY2VhYTA2N2ViXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+for(var i = 0; i < listaFilmes.length; i++){
+    document.write("<img src=" + listaFilmes[i] + ">")
+}
+
+
+
