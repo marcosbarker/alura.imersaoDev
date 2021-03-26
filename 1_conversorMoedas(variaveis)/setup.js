aa1bf6ffa7e920907d76da9303adcd6e10e9b49c
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-03-26 10:02:06 -0300

    docs: rename file

---
+++ 1_conversorMoedas(variaveis)/setup.js
@@ -0,0 +1,11 @@
+var valorEmDolarTexto = prompt ("Qual o valor em dolar para converter?")
+
+var valorEmDolarNumero = parseFloat (valorEmDolarTexto)
+
+alert (valorEmDolarNumero)
+
+var valorEmReal = valorEmDolarNumero * 5.51
+
+var valorEmRealDEcimal = valorEmReal.toFixed(2)
+
+alert (valorEmReal)
