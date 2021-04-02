8cc536a1617bc9d21fd07fbf8b4bfbf6b617252a
marcosbarker <marcos_barker@hotmail.com>
2021-04-02 10:49:04 -0300

    add cardsPlayer

--- 9_superTrunfo(js)/setup.js
+++ 9_superTrunfo(js)/setup.js
@@ -28,6 +28,57 @@ var cartaAnaCarolina = {
     }
 }
 
+var cartaLol = {
+    nome: "Caitlyn",
+    imagem: "http://1.bp.blogspot.com/-K7CbqWc1-p0/VLc98v85s0I/AAAAAAAABqk/-ZB684VVHbg/s1600/Caitlyn_OriginalSkin.jpg",
+    atributos: {
+        ataque: 95,
+        defesa: 40,
+        magia: 10
+    }
+}
+
+var cartaNaruto = {
+    nome: "Naruto",
+    imagem: "https://conteudo.imguol.com.br/c/entretenimento/16/2017/06/27/naruto-1498593686428_v2_450x337.png",
+    atributos: {
+        ataque: 80,
+        defesa: 60,
+        magia: 100
+    }
+}
+
+var cartaHarry = {
+    nome: "Harry Potter",
+    imagem: "https://sm.ign.com/ign_br/screenshot/default/89ff10dd-aa41-4d17-ae8f-835281ebd3fd_49hp.jpg",
+    atributos: {
+        ataque: 70,
+        defesa: 50,
+        magia: 95
+    }
+}
+
+var cartaBatman = {
+    nome: "Batman",
+    imagem: "https://assets.b9.com.br/wp-content/uploads/2020/09/Batman-issue86-heder-1280x677.jpg",
+    atributos: {
+        ataque: 95,
+        defesa: 70,
+        magia: 0
+    }
+}
+
+var cartaMarvel = {
+    nome: "Capitã Marvel",
+    imagem: "https://cinepop.com.br/wp-content/uploads/2018/09/capitamarvel21.jpg",
+    atributos: {
+        ataque: 90,
+        defesa: 80,
+        magia: 0
+    }
+}
+
+
  var cartaMaquina   
  var cartaJogador
 
